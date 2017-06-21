5562e8b565ffa1e813a6b92483db8c12b74931ea
Petard <scumpetard@gmail.com>
2017-06-21 09:24:49 +0800

    AAAAssss ^ _ ^

--- app/Http/Middleware/MustOnRealIp.php
+++ app/Http/Middleware/MustOnRealIp.php
@@ -29,8 +29,8 @@ class MustOnRealIp
                 $endTime = strtotime($ip->client->end_ip);
 
                 if(
-//                    Tools::check($ip->ip,$request->getClientIp())
-                    Tools::check($ip->ip,'192.168.0.255')
+                    Tools::check($ip->ip,$request->getClientIp())
+//                    Tools::check($ip->ip,'192.168.0.255')
                     && ($startTime < time() && time() < $endTime)
                 ) {
                     Session::put('clientRealIp', $ip->ip);
